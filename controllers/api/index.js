dbe4612f30a0441d2f0e7a20dcffd4d86828b308
dds git <deeann@stelzl.us>
2022-09-30 09:39:33 -0400

    seeded users, dogs, locations and started allDogs and dog handlebars pages

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const dogRoutes = require('./dogRoutes');
+const locationRoutes = require('./locationRoutes');
 
 router.use('/users', userRoutes);
+router.use('/dogs', dogRoutes);
+router.use('/locations', locationRoutes);
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,102 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Dog, Location } = require('../models');
 
-const userData = require('./userData.json');
+// const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
+  await Location.create({
+    name: 'Pineville',
+  });
+  await Location.create({
+    name: 'South End',
+  });
+  await Location.create({
+    name: 'North Side',
+  });
+  await Location.create({
+    name: 'Steel Creek',
   });
 
+  await User.create({
+    name: 'Ralph',
+    email: 'ralph@aol.com',
+    password: '12dfghdfh345678',
+    location_id: [1],
+  });
+  await User.create({
+    name: 'Jenny',
+    email: 'jenny@jenny.com',
+    password: '125dfg6778',
+    location_id: [2],
+  });
+  await User.create({
+    name: 'Matt',
+    email: 'matt@me.com',
+    password: 'ssdhfgfgfoushfi',
+    location_id: [3],
+  });
+  await User.create({
+    name: 'Juanita',
+    email: 'jjy@comcast.net',
+    password: 'lovemypup56',
+    location_id: [1],
+  });
+  await User.create({
+    name: 'John',
+    email: 'jpm@me.com',
+    password: 'create$U98765',
+    location_id: [4],
+  });
+  await User.create({
+    name: 'Jill',
+    email: 'jill@yahoo.com',
+    password: 'ght56dfh78',
+    location_id: [3],
+  });
+  await Dog.create({
+    dog_name: 'Buddy',
+    about: 'ihsdkfghsli',
+    owner_id: 1,
+  });
+  await Dog.create({
+    dog_name: 'Sparky',
+    about: 'lorem sdjkhfsdj',
+    owner_id: 2,
+  });
+  await Dog.create({
+    dog_name: 'Rover',
+    about: 'sdhfiuhsdrlirgh',
+    owner_id: 2,
+  });
+  await Dog.create({
+    dog_name: 'Max',
+    about: 'dfiugsrkrugfsh',
+    owner_id: 3,
+  });
+  await Dog.create({
+    dog_name: 'Dexter',
+    about: 'dhfhzsdgf',
+    owner_id: 4,
+  });
+  await Dog.create({
+    dog_name: 'Brady',
+    about: 'fiughgrh',
+    owner_id: 5,
+  });
+  await Dog.create({
+    dog_name: 'Zeus',
+    about: 'foghlidrbuhgdijg',
+    owner_id: 6,
+  });
+  await Dog.create({
+    dog_name: 'Bentley',
+    about: 'djhfiseruhindrgh',
+    owner_id: 5,
+  });
+
+  console.log('data seeded');
   // for (const project of projectData) {
   //   await Project.create({
   //     ...project,
